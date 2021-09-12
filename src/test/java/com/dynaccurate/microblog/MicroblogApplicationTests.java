ef58b1d7604298740bbf911b78a7d99e9ffc6551
Carlos Eduardo Artioli <caduartioli@gmail.com>
2021-09-11 21:10:01 -0300

    Initial commit

---
+++ src/test/java/com/dynaccurate/microblog/MicroblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynaccurate.microblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
